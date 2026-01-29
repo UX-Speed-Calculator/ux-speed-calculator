808c6b4d96586612a05b4740197e24d36e513289
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2026-01-29 16:14:21 -0500

    blocking out of descriptions and future canvas

--- src/routes/step6.tsx
+++ src/routes/step6.tsx
@@ -3,7 +3,20 @@ import ControlsGrid from '../components/ControlsGrid';
 export default function Step6() {
   return (
     <main class="mx-auto p-4 text-center">
-      <h1 class="max-6-xs text-6xl uppercase">Step 6</h1>
+      <section class="info-and-canvas grid grid-cols-4 gap-1">
+        <section class="col-span-1 flex flex-col">
+          <h1 class="text-[2rem] uppercase">Step 1</h1>
+          <section class="mt-5">
+            <h2>What Matters to My Business?</h2>
+            <p>
+              Conversion Rate origins: performance, presentation quality, SEO, availability, etc.
+            </p>
+          </section>
+        </section>
+        <div class="future-canvas col-span-3">
+          <img src="3d_diagram_placeholder.png" class="w-full" />
+        </div>
+      </section>
       <ControlsGrid />
     </main>
   );
