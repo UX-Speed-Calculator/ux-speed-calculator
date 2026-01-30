b994f184383c501387dd62fb261bc30b9a9fa92b
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2026-01-29 21:31:29 -0500

    styling adjustment and reaction to routing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,32 +1,34 @@
 import { useLocation } from '@solidjs/router';
+import { A } from '@solidjs/router';
 
 function Nav() {
   const location = useLocation();
-  const active = (path: string) =>
-    path === location.pathname ? 'border-sky-600' : 'border-transparent hover:border-sky-600';
+
   return (
     <nav class="flex flex-col items-center bg-stone-900">
       <ul class="container flex items-center justify-evenly p-2">
-        <li class={`font-[1000] ${active('/step1')} `}>
-          <a href="/step1">Step1</a>
+        <li class={`font-[1000]`}>
+          <A href="/" end>
+            Level 1
+          </A>
         </li>
-        <li class={`font-[1000] ${active('/step2')} `}>
-          <a href="/step2">Step2</a>
+        <li class={`font-[1000]`}>
+          <A href="/step2">Step2</A>
         </li>
-        <li class={`font-[1000] ${active('/step3')} `}>
-          <a href="/step3">Step3</a>
+        <li class={`font-[1000]`}>
+          <A href="/step3">Step3</A>
         </li>
-        <li class={`font-[1000] ${active('/step4')} `}>
-          <a href="/step4">Step4</a>
+        <li class={`font-[1000]`}>
+          <A href="/step4">Step4</A>
         </li>
-        <li class={`font-[1000] ${active('/step5')} `}>
-          <a href="/step5">Step5</a>
+        <li class={`font-[1000]`}>
+          <A href="/step5">Step5</A>
         </li>
-        <li class={`font-[1000] ${active('/step6')} `}>
-          <a href="/step6">Step6</a>
+        <li class={`font-[1000]`}>
+          <A href="/step6">Step6</A>
         </li>
-        <li class={`font-[1000] ${active('/step7')} `}>
-          <a href="/step7">Step7</a>
+        <li class={`font-[1000]`}>
+          <A href="/step7">Step7</A>
         </li>
       </ul>
     </nav>
